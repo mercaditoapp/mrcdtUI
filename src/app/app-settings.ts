bc4518041e5a516b2c9f6b551ef4b012843e0315
Arturo Aguirre <Arturo.Aguirre.consultant@nielsen.com>
2019-02-22 20:25:30 -0500

    Fixing Absolut URLs

--- src/app/app-settings.ts
+++ src/app/app-settings.ts
@@ -1,4 +1,4 @@
 export class AppSettings {
     //public static API_ENDPOINT='http://localhost:8080';
-    public static API_ENDPOINT='http://34.210.212.235:8080/mrcdtAPI';
+    public static API_ENDPOINT='http://18.220.109.49:8080/mrcdtAPI';
 }
