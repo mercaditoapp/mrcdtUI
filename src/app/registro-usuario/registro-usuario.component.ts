b2424cd6e0f8a581dfeaf108866cc006e017fb00
lluvi <lluvi@DESKTOP-GIP9VUV>
2019-02-26 17:54:59 -0500

    mejoras Registro Usuario/ index renombres

--- src/app/registro-usuario/registro-usuario.component.ts
+++ src/app/registro-usuario/registro-usuario.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro-usuario.component.css']
 })
 export class RegistroUsuarioComponent implements OnInit {
+  nombre: string;
+  apeidos: string;
+  email: string;
+  contraseña: string;
+  telefono: string;
+  Cuidad: string;
+  Estado: string;
+  sexo;
 
-  constructor() { }
+  constructor() {
+
+    this.nombre= "Lluvia";
+    this.apeidos= "Saucedo";
+    this.email="lluviaayonectili@gmail.com";
+    this.contraseña="29062000";
+    this.telefono="5510829238";
+    this.Cuidad="Caucun";
+    this.Estado="Quintana Roo";
+    this.sexo="mujer";
+   }
 
   ngOnInit() {
+
   }
 
+ 
 }
